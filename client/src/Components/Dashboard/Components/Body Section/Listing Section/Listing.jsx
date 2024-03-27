ff26d9e4b43fb858b9c60f974d3fab9d884f6392
kathosseni <ossenimalikath@gmail.com>
2024-03-27 12:04:42 +0100

    End of Top Dash

--- client/src/Components/Dashboard/Components/Body Section/Listing Section/Listing.jsx
+++ client/src/Components/Dashboard/Components/Body Section/Listing Section/Listing.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import "./Listing.css"
 
+import { BsArrowRightShort } from 'react-icons/bs'
+import { AiFillHeart } from 'react-icons/ai'
+
+import img2 from '../../../Assets/img2.png'
+
 const Listing = () => {
   return (
-    <div>Listing</div>
+    <div className='listingSection'>
+      <div className="heading flex">
+        <h1>My Listings</h1>
+        <button className="btn flex">
+          See All <BsArrowRightShort className='icon' />
+        </button>
+      </div>
+
+      <div className="secContainer flex">
+        <div className="singleItem">
+          <AiFillHeart className='icon' />
+          <img src={img2} alt="" />
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/Components/Dashboard/Components/Body Section/Top Section/Top.jsx
+++ client/src/Components/Dashboard/Components/Body Section/Top Section/Top.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import "./Top.css"
+import React from "react";
+import "./Top.css";
 
-import { BiSearchAlt } from 'react-icons/bi'
-import { TbMessageCircle } from 'react-icons/tb'
-import { MdNotificationsOutline } from 'react-icons/md'
+import { BiSearchAlt } from "react-icons/bi";
+import { TbMessageCircle } from "react-icons/tb";
+import { MdOutlineNotificationsNone } from "react-icons/md";
+import { BsArrowRightShort } from "react-icons/bs";
+
+import user3 from "../../../Assets/user3.png";
+import img1 from "../../../Assets/img1.png";
+import vid from "../../../Assets/vid.mp4";
 
 const Top = () => {
   return (
-    <div className='topSection'>
+    <div className="topSection">
       <div className="headerSection flex">
         <div className="title">
           <h1>Welcome to Share.</h1>
@@ -15,20 +20,78 @@ const Top = () => {
         </div>
 
         <div className="searchBar flex">
-          <input type="text" placeholder='Search Dashboard'/>
-          <BiSearchAlt classname='icon' />
+          <input type="text" placeholder="Search Dashboard" />
+          <BiSearchAlt className="icon" />
         </div>
 
         <div className="adminDiv flex">
-          <TbMessageCircle className='icon'/>
-          <MdNotificationsOutline className='icon'/>
+          <TbMessageCircle className="icon" />
+          <MdOutlineNotificationsNone className="icon" />
           <div className="adminImage">
-            {/* <img src={img} alt="Admin Image" /> */}
+            <img src={user3} alt="Admin Image" />
+          </div>
+        </div>
+      </div>
+
+      <div className="cardSection flex">
+        <div className="rigthCard flex">
+          <h1>Create and share extraordinary products</h1>
+          <p>
+            The world's fast growing industry today are tech and digital
+            connection
+          </p>
+
+          <div className="buttons flex">
+            <button className="btn">Explore More </button>
+            <button className="btn transparent">Top Seller</button>
+          </div>
+
+          <div className="videoDiv">
+            <video src={vid} autoPlay loop muted></video>
+          </div>
+        </div>
+
+        <div className="leftCard fle">
+          <div className="main flex">
+            <div className="textDiv">
+              <h1>My Stat</h1>
+
+              <div className="flex">
+                <span>
+                  Today <br /> <small>4 Files</small>
+                </span>
+                <span>
+                  This Month <br /> <small>175 Files</small>
+                </span>
+              </div>
+
+              <span className="flex link">
+                Go to my orders <BsArrowRightShort className="icon" />
+              </span>
+            </div>
+
+            <div className="imgDiv">
+              <img src={img1} alt="Image Name" />
+            </div>
+
+            {/* <div className="sideBarCard">
+              <BsQuestionCircle className="icon" />
+              <div className="cardContent">
+                <div className="circle1"></div>
+                <div className="circle2"></div>
+
+                <h3>Help center</h3>
+                <p>
+                  Having trouble in Share, please contact us for more questions.
+                </p>
+                <button className="btn"> Go to help center</button>
+              </div>
+            </div> */}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Top
+export default Top;
